4bb8d983a1c4b2e770cf38a0415cd3412066ed75
Katie Williams <kawilliams8@gmail.com>
2020-12-12 12:17:56 -0700

    Add third useState case, adding an incremented number into an array of nums

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 function Multiplier () {
     const [num, setNum] = useState({ myNum: 1 });
     const [num2, setNum2] = useState({ myNum2: 2 });
+    const [nums, setNums] = useState([0, 1]);
     //useState returns the current state variable and a function to update it
     //Use it as you would this.setState()
     //Requires one argument, the initial state: 1
     //Can be object, or any data type
+
+    const numsList = nums.map((num, index) => {
+        return index == nums.length - 1 ? num : num += ', ';
+    });
+
+    const nextNum = nums[nums.length - 1] + 1;
 
     return (
         <div>
@@ -17,6 +24,11 @@ function Multiplier () {
                 setNum({ myNum: num.myNum * 10 });
                 setNum2({ myNum2: num2.myNum2 * 10 });
             }}>Multiply by 10</button>
+            <h1>List of numbers: {numsList}</h1>
+            <h1>Next number: {nextNum}</h1>
+            <button onClick={() => {
+                setNums([...nums, nextNum]);
+            }}>Add to list</button>
         </div>
     );
 }
