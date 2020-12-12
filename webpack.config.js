ce0b137d87434b50ecb791706798462d59fe947e
Katie Williams <kawilliams8@gmail.com>
2020-12-12 10:26:10 -0700

    Update webpack settings for /dist, first React on the DOM

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "dist"),
     publicPath: "/",
     filename: "bundle.js",
   },
