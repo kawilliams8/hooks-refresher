4863bd1b0a65b43dcd4ec9e31b6e106094064ad1
Katie Williams <kawilliams8@gmail.com>
2020-12-14 15:07:52 -0700

    Add historyApiFallback to webpack config to re-direct 404s to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        historyApiFallback: true
     },
     module: {
         rules: [
