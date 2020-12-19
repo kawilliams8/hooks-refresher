7f9b01addfac67253ce7cde4d10b5495c7de596c
Katie Williams <kawilliams8@gmail.com>
2020-12-19 09:52:23 -0700

    Add onClick focus and blur of the inputRef

--- src/NameRefs.js
+++ src/NameRefs.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const NameRefs = () => {
     const [name, setName] = useState('');
-    const [count, setCount] = useState(0);
+    const count = useRef(1);
+    const inputRef = useRef();
 
     //useEffect(() => {
     //infinite loop while trying to count the renders
@@ -10,11 +11,32 @@ const NameRefs = () => {
     //setCount(prevCount => prevCount + 1);
     //});
 
+    //Refs persist, but do not trigger a re-render when changed
+    //useRef returns an object: {current: default prop}
+
+    useEffect(() => {
+        //Updates on each component re-render
+        //count.current persists across renders
+        //Does not trigger own re-render
+        count.current = count.current + 1;
+    });
+
+    const focus = () => {
+        //inputRef is the DOM node
+        inputRef.current.focus();
+    };
+
+    const blur = () => {
+        inputRef.current.blur();
+    };
+
     return (
         <>
-            <input value={name} onChange={e => setName(e.target.value)} />
+            <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
             <div>Name: {name}</div>
-            <h1>{count}</h1>
+            <h1>I rendered {count.current} times.</h1>
+            <button onClick={focus}>Focus Input</button>
+            <button onClick={blur}>Blur Input</button>
         </>
     );
 };
