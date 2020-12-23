5c0649c4f54c2bd6e24bdd3832b64530cf237ee1
Katie Williams <kawilliams8@gmail.com>
2020-12-23 10:55:20 -0700

    Add default to switch, for non-matching actions

--- src/CounterWithReducer.js
+++ src/CounterWithReducer.js
@@ -14,6 +14,8 @@ const CounterWithReducer = () => {
                 return { count: state.count + 1 };
             case 'decrement':
                 return { count: state.count - 1 };
+            default:
+                return state;
         }
     };
     const [state, dispatch] = useReducer(reducer, { count: 0 });
