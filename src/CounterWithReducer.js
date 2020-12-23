f82b2a6449e3d8544b7b04b5c75d439e0d228b63
Katie Williams <kawilliams8@gmail.com>
2020-12-23 10:53:51 -0700

    Dispatch increment/decrement actions to the reducer

--- src/CounterWithReducer.js
+++ src/CounterWithReducer.js
@@ -1,32 +1,35 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer } from 'react';
 
 const CounterWithReducer = () => {
-    const [count, setCount] = useState(0);
-
     //useReducer takes a function to call on state, and the default state
-    //Default state is typically an object, to manage higher levels of complexity
-    //Since default is object, the first returned is the larger idea of "state"
-    //If we passed something simple, like just 0, the first returned could just be "count"
-    //Dispatch() is the func called to update state
+    //Default state is typically an object, to manage higher levels of state complexity
+    //Since the default is object, the first returned from useReducer is the larger idea of "state"
+    //If we passed something simple, like 0, the first returned from useReducer could just be "count"
+    //Dispatch() is the func called to update state, given some parameters
     //The reducer needs current state, and the action to execute upon it
 
     const reducer = (state, action) => {
-
+        switch (action.type) {
+            case 'increment':
+                return { count: state.count + 1 };
+            case 'decrement':
+                return { count: state.count - 1 };
+        }
     };
     const [state, dispatch] = useReducer(reducer, { count: 0 });
 
     const increment = () => {
-        setCount((prevCount) => prevCount + 1);
+        dispatch({ type: 'increment' });
     };
 
     const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
+        dispatch({ type: 'decrement' });
     };
 
     return (
         <>
             <button onClick={increment}>+</button>
-            <h2>Count: {count}</h2>
+            <h2>Count: {state.count}</h2>
             <button onClick={decrement}>-</button>
         </>
     );
