3fd18631787f9ba43c921cf1294cbe57c65ab0a1
Katie Williams <kawilliams8@gmail.com>
2020-12-23 11:34:39 -0700

    Move ACTIONS, newTodo and reducer out of the component, incorporate payload into action

--- src/TodosWithReducer.js
+++ src/TodosWithReducer.js
@@ -1,19 +1,35 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
+
+const ACTIONS = {
+    ADD_TODO: 'add-todo'
+};
+const newTodo = (name) => {
+    return { id: Date.now(), name: name, complete: false };
+};
+const reducer = (todos, action) => {
+    switch (action.type) {
+        case ACTIONS.ADD_TODO:
+            return [...todos, newTodo(action.payload.name)];
+        default:
+            return todos;
+    }
+};
 
 const TodosWithReducer = () => {
-    const ACTIONS = {
-
-    };
-    const reducer = (todos, action) => {
-        switch (action.type) {
-            default: return todos;
-        }
-    };
     const [todos, dispatch] = useReducer(reducer, []);
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+        setName('');
+    };
+
+    console.log('todos', todos);
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             </form>
         </>
