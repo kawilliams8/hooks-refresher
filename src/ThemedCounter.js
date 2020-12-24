3c2143e8674956c550ee135c187652ed32b1a251
Katie Williams <kawilliams8@gmail.com>
2020-12-23 18:19:16 -0700

    Add useMemo and useCallback comparison comment

--- src/ThemedCounter.js
+++ src/ThemedCounter.js
@@ -11,6 +11,7 @@ const ThemedCounter = () => {
         //getNums is re-created every time ThemedCounter is rendered, which changes getNums, which calls the List useEffect
         //With useCallback, it now only updates getNums when [num] changes
         //Similar to useMemo, except for the return value. Here, it returns the whole function.
+        //useCallback and useMemo both exist to handle referential equality issues, when output is a useEffect dependency
         return [num + incrementor, num + incrementor + 1, num + incrementor + 2];
     }, [num]);
     const theme = {
