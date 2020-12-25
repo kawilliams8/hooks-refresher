a74c0c8c60bef4fa89ab0e99e3aece569e2d55cb
Katie Williams <kawilliams8@gmail.com>
2020-12-24 18:26:45 -0700

    Finish custom useLocalStorage hook that relies on useState/useEffect

--- src/Custom.js
+++ src/Custom.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useLocalStorage from './useLocalStorage';
 
 const Custom = () => {
-    const [name, setName] = useState('');
+    const [name, setName] = useLocalStorage('name', '');
 
     return (
         <input
--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const useLocalStorage = () => {
-    const [value, setValue] = useState();
+const getStored = (key, initialValue) => {
+    const storedValue = JSON.parse(localStorage.getItem(key));
+    if (storedValue) {return storedValue;}
+    if (initialValue instanceof Function) {return initialValue();}
+    return initialValue;
+};
+
+const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+        return getStored(key, initialValue);
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value]);
+
     return [value, setValue];
 };
 
